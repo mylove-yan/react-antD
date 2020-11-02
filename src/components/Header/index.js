e7c3d5af2b8426d949a4de09e0eb6935bb63a190
马誌浩 <mazh@emrubik.com>
2020-11-02 13:09:57 +0800

    #通用模块修改

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -36,29 +36,40 @@ export default class Header extends React.Component {
     }
 
 
-    render() {
+    render(){
+        const menuType = this.props.menuType;
         return (
             <div className="header">
                 <Row className="header-top">
-                    <Col span={24}>
+                    {
+                        menuType?
+                            <Col span="6" className="logo">
+                                <img src="/assets/logo-ant.svg" alt=""/>
+                                <span>江西水务</span>
+                            </Col>:''
+                    }
+                    <Col span={menuType?18:24}>
                         <span>欢迎，{this.state.userName}</span>
                         <a href="#">退出</a>
                     </Col>
                 </Row>
-                <Row className="breadcrumb">
-                    <Col span={4} className="breadcrumb-title">
-                         首页
-                    </Col>
-                    <Col span={20} className="weather">
-                        <span className="date">{this.state.sysTime}</span>
-                        <span className="weather-img">
-                                    <img src={this.state.dayPictureUrl} alt=""/>
+                {
+                    menuType?'':
+                        <Row className="breadcrumb">
+                            <Col span="4" className="breadcrumb-title">
+                                { this.props.menuName }
+                            </Col>
+                            <Col span="20" className="weather">
+                                <span className="date">{this.state.sysTime}</span>
+                                <span className="weather-img">
+                                    <img src={this.state.dayPictureUrl} alt="" />
                                 </span>
-                        <span className="weather-detail">
+                                <span className="weather-detail">
                                     {this.state.weather}
                                 </span>
-                    </Col>
-                </Row>
+                            </Col>
+                        </Row>
+                }
             </div>
         );
     }
